a2ddb6165442e7476a91e16a43a5c5bb6bb4c5b7
kazrkb <kazrkb@gmail.com>
2025-07-24 01:57:40 +0600

    added a user to the database

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,38 @@ import './App.css'
 
 function App() {
 
+  const handleAddUser = event => {
+    event.preventDefault()
+    const form = event.target
+    const name = form.name.value
+    const email = form.email.value
+    const user = {name,email}
+    console.log(user)
+
+    fetch('http://localhost:5000/users',{
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(user)
+    })
+    .then(res=> res.json())
+    .then(data=>{
+      console.log(data)
+    })
+
+  }
+
   return (
     <>
-      <h1>Vite + React</h1>
+      <h1>Simple Crud</h1>
+      <form action="" onSubmit={handleAddUser}>
+        <input type="text" name='name' />
+        <br />
+        <input type="email" name='email' />
+        <br />
+        <input type="submit" value="Add User" />
+      </form>
     </>
   )
 }
